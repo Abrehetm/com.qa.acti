5a82e95d402c60645360522dde22487695b58415
ktesf <ktesf@Naomi>
2023-05-01 12:23:11 -0500

    added constructor to enter page

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -1,5 +1,7 @@
 package com.acti.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 import com.acti.base.DriverScript;
 
 /*
@@ -11,5 +13,16 @@ import com.acti.base.DriverScript;
  */
 
 public class EnterPage extends DriverScript{
+	
+//***************************page Locators***************************//
 
+//***************************page Initialization********************//
+	
+	public EnterPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+//***************************page Methods**************************//
 }
