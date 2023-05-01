9966822865f3fcb809e96081a1ce00d31ce46bb0
ktesf <ktesf@Naomi>
2023-05-01 12:11:26 -0500

    Added author details to enterpage

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -2,6 +2,14 @@ package com.acti.pages;
 
 import com.acti.base.DriverScript;
 
+/*
+ * Name: EnterPage
+ * Description: manages the locators  and methods of enter page
+ * Developed By: Abrehet
+ * Reviewed By: Shantosh
+ * Date reviewed : 05-01-2023
+ */
+
 public class EnterPage extends DriverScript{
 
 }
